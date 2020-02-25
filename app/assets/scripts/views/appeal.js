5a863787cb4499ed1199bc8980178cde15c5c449
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-02-25 15:55:02 +0100

    Added checkbox and some fields for Appeal page

--- app/assets/scripts/views/appeal.js
+++ app/assets/scripts/views/appeal.js
@@ -10,16 +10,18 @@ import {
 import {
   FormInput,
   FormTextarea,
-  FormRadioGroup,
+  FormCheckbox,
   FormSelect,
   FormError
 } from '../components/form-elements/';
+import Fold from '../components/fold';
 import App from './app';
 import Select from 'react-select';
 import _set from 'lodash.set';
 import _cloneDeep from 'lodash.clonedeep';
 import { appealTypes, appealTypeOptions } from '../utils/appeal-type-constants';
 import { disasterType } from '../utils/field-report-constants';
+import { appealStatusOptions } from '../utils/utils';
 import { showGlobalLoading, hideGlobalLoading } from '../components/global-loading';
 
 class Appeal extends React.Component {
@@ -27,6 +29,7 @@ class Appeal extends React.Component {
     super(props);
     this.state = {
       appeal: null,
+      appealStatusOptionList: appealStatusOptions.slice(1),
       appealTypeOptionList: appealTypeOptions.slice(1),
       disasterTypeOptionList: disasterType.slice(1),
       disasterType: -1,
@@ -46,7 +49,7 @@ class Appeal extends React.Component {
     if (nextProps.appeal.fetched) {
       hideGlobalLoading();
       this.setState({
-        appeal: nextProps.appeal.data.results[0],
+        appeal: nextProps.appeal.data.results[0]
       });
     }
   }
@@ -54,6 +57,12 @@ class Appeal extends React.Component {
   // onSubmit (e) {
 
   // }
+
+  onCheckChange (field, isTrue) {
+    let appeal = _cloneDeep(this.state.appeal);
+    _set(appeal, field, !isTrue);
+    this.setState({appeal});
+  }
 
   onFieldChange (field, fieldType, e) {
     let val = e && e.target ? e.target.value : e;
@@ -88,13 +97,13 @@ class Appeal extends React.Component {
 
   renderContent () {
     const { fetched } = this.props.appeal;
-    const appeal = fetched ? this.props.appeal.data.results[0] : undefined;
 
     if (fetched) {
+      console.log(this.state.appeal);
       return (
         <React.Fragment>
-          {this.renderHelmet(appeal)}
-          {this.renderBody(appeal)}
+          {this.renderHelmet()}
+          {this.renderBody()}
         </React.Fragment>
       );
     } else {
@@ -102,90 +111,229 @@ class Appeal extends React.Component {
     }
   }
 
-  renderHelmet (appeal) {
-    const helmetContent = appeal
-      ? (
+  renderHelmet () {
+    return (
+      <React.Fragment>
         <Helmet>
-          <title>IFRC Go - Appeal - {appeal.name}</title>
+          <title>IFRC Go - Appeal - {this.state.appeal.name ? this.state.appeal.name : ''}</title>
         </Helmet>
-      )
-      : (<div></div>);
-
-    return helmetContent;
+      </React.Fragment>
+    );
   }
 
-  renderHeader (appeal) {
-    const headerContent = appeal
-      ? (
-        <header className='inpage__header'>
-          <div className='inner'>
-            <h1 className='inpage__title'>{appeal.name}</h1>
-          </div>
-        </header>
-      )
-      : (<div></div>);
-
-    return headerContent;
+  renderHeader () {
+    return (
+      <header className='inpage__header'>
+        <div className='inner'>
+          <h1 className='inpage__title'>{this.state.appeal.name}</h1>
+        </div>
+      </header>
+    );
   }
 
-  renderBody (appeal) {
+  renderBody () {
+    console.log(this.state.appeal);
     return (
       <section className='inpage'>
-        {this.renderHeader(appeal)}
+        {this.renderHeader()}
 
         <div className='inpage__body'>
           <div className='inner'>
             <form className='form'>
-              <div className='form-group'>
-                <div className='form__inner_body'>
-                  <FormInput
-                    label='Aid'
-                    type='text'
-                    name='aid'
-                    id='aid'
-                    value={this.state.appeal.aid}
-                    onChange={this.onFieldChange.bind(this, 'aid', '')}
-                    // description={'very description'}
-                  >
-                    <FormError
-                      errors={this.state.errors}
-                      property='start_date'
+              <Fold title='General'>
+                <FormInput
+                  label='Aid'
+                  type='text'
+                  name='aid'
+                  id='aid'
+                  value={this.state.appeal.aid}
+                  onChange={this.onFieldChange.bind(this, 'aid', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='aid'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Code'
+                  type='text'
+                  name='code'
+                  id='code'
+                  value={this.state.appeal.code}
+                  onChange={this.onFieldChange.bind(this, 'code', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='code'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Number of beneficiaries'
+                  type='number'
+                  name='num_beneficiaries'
+                  id='num_beneficiaries'
+                  value={this.state.appeal.num_beneficiaries}
+                  onChange={this.onFieldChange.bind(this, 'num_beneficiaries', 'number')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='num_beneficiaries'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Amount requested'
+                  type='number'
+                  name='amount_requested'
+                  id='amount_requested'
+                  value={this.state.appeal.amount_requested}
+                  onChange={this.onFieldChange.bind(this, 'amount_requested', 'number')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='amount_requested'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Amount funded'
+                  type='number'
+                  name='amount_funded'
+                  id='amount_funded'
+                  value={this.state.appeal.amount_funded}
+                  onChange={this.onFieldChange.bind(this, 'amount_funded', 'number')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='amount_funded'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Start date'
+                  type='date'
+                  name='start_date'
+                  id='start_date'
+                  value={this.state.appeal.start_date}
+                  onChange={this.onFieldChange.bind(this, 'start_date', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='start_date'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='End date'
+                  type='date'
+                  name='end_date'
+                  id='end_date'
+                  value={this.state.appeal.end_date}
+                  onChange={this.onFieldChange.bind(this, 'end_date', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='end_date'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Name'
+                  type='text'
+                  name='name'
+                  id='name'
+                  value={this.state.appeal.name}
+                  onChange={this.onFieldChange.bind(this, 'name', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='name'
+                  />
+                </FormInput>
+
+                <FormInput
+                  label='Sector'
+                  type='text'
+                  name='sector'
+                  id='sector'
+                  value={this.state.appeal.sector}
+                  onChange={this.onFieldChange.bind(this, 'sector', '')}
+                  // description={'very description'}
+                >
+                  <FormError
+                    errors={this.state.errors}
+                    property='sector'
+                  />
+                </FormInput>
+
+                <FormCheckbox
+                  label='Needs confirmation'
+                  name='needs_confirmation'
+                  id='needs_confirmation'
+                  value={this.state.appeal.needs_confirmation}
+                  checked={this.state.appeal.needs_confirmation}
+                  onChange={this.onCheckChange.bind(this, 'needs_confirmation', this.state.appeal.needs_confirmation)}
+                  // description={o.description}
+                />
+
+                <div className='form__group'>
+                  <div className='form__inner_header'>
+                    <label className='form__label'>Status</label>
+                  </div>
+                  <div className='form__inner_body'>
+                    <Select
+                      placeholder='Select a status'
+                      name='status'
+                      id='status'
+                      options={this.state.appealStatusOptionList}
+                      value={this.state.appeal.status}
+                      onChange={this.onFieldChange.bind(this, 'status', 'dropdown')}
                     />
-                  </FormInput>
+                  </div>
                 </div>
-              </div>
 
-              <div className='form-group'>
-                <div className='form__inner-header'>
-                  <label className='form__label'>Disaster Type</label>
+                <div className='form__group'>
+                  <div className='form__inner_header'>
+                    <label className='form__label'>Disaster Type</label>
+                  </div>
+                  <div className='form__inner_body'>
+                    <Select
+                      placeholder='Select a disaster type'
+                      name='disaster-type'
+                      id='disaster-type'
+                      options={this.state.disasterTypeOptionList}
+                      value={this.state.appeal.dtype.id}
+                      onChange={this.onFieldChange.bind(this, 'disasterType', '')}
+                    />
+                  </div>
                 </div>
-                <div className='form_inner-body'>
-                  <Select
-                    placeholder='Select a disaster type'
-                    name='disaster-type'
-                    id='disaster-type'
-                    options={this.state.disasterTypeOptionList}
-                    value={this.state.appeal.dtype.id}
-                    onChange={this.onFieldChange.bind(this, 'disasterType', '')}
-                  />
-                </div>
-              </div>
 
-              <div className='form-group'>
-                <div className='form__inner-header'>
-                  <label className='form__label'>Appeal Type</label>
+                <div className='form__group'>
+                  <div className='form__inner_header'>
+                    <label className='form__label'>Appeal Type</label>
+                  </div>
+                  <div className='form__inner_body'>
+                    <Select
+                      placeholder='Select an Appeal type'
+                      name='atype'
+                      id='atype'
+                      options={this.state.appealTypeOptionList}
+                      value={this.state.appeal.atype}
+                      onChange={this.onFieldChange.bind(this, 'atype', 'dropdown')}
+                    />
+                  </div>
                 </div>
-                <div className='form_inner-body'>
-                  <Select
-                    placeholder='Select a disaster type'
-                    name='disaster-type'
-                    id='disaster-type'
-                    options={this.state.appealTypeOptionList}
-                    value={this.state.appeal.atype}
-                    onChange={this.onFieldChange.bind(this, 'atype', 'dropdown')}
-                  />
-                </div>
-              </div>
+              </Fold>
             </form>
 
           </div>
@@ -204,9 +352,8 @@ class Appeal extends React.Component {
 }
 
 // TODO: make an input for all other Appeal fields
-// dropdown: atype, status, event, country
-// text: aid, code, sector, region (not editable)
-// num: num beneficiaries, amount requested, funded
+// dropdown: event, country
+// text: region (not editable)
 // date: start date, end date
 // checkbox: needs confirmation
 // list: appeal documents
